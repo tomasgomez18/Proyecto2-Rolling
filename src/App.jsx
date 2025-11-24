9eaa2f0d37b2b7bfbcb6f417d14d7caa901b3feb
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-11-24 10:51:44 -0300

    feat: agregar snippet rafc y configurar React Router
    -Implementar snippet rafc (React Arrow Function Component) para creación rápida de componentes
    - Agregar componente Menu como elemento compartido en la aplicación
    - Configurar ruta principal '/' que renderiza el componente Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import { BrowserRouter,Routes,Route } from "react-router"
+import Menu from "./Componentes/Shared/Menu/Menu"
+import { Home } from './Componentes/Views/Home/Home'
 import './App.css'
 
 function App() {
@@ -6,6 +9,12 @@ function App() {
 
   return (
     <>
+    <BrowserRouter>
+       <Menu />
+       <Routes>
+         <Route path="/" element={<Home/>} />
+       </Routes>
+    </BrowserRouter>
     </>
   )
 }
