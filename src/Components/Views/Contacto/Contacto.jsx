4b20da789891f63d42c23122790016ba63bc8923
Giovanna Ianiero <Ianierogiovanna@gmail.com>
2025-11-28 16:21:36 -0300

    modifica estructura para que sea responsive

--- src/Components/Views/Contacto/Contacto.jsx
+++ src/Components/Views/Contacto/Contacto.jsx
@@ -53,11 +53,8 @@ const Contacto = () => {
                             backdropFilter: "blur(6px)",
                             color: "white",
                         }}
-
-
                     >
                         <h2 className="titulo-glow">Rolling Motors</h2>
-
                     </div>
                     <form
                         ref={form}
@@ -68,38 +65,32 @@ const Contacto = () => {
                             backdropFilter: "blur(6px)",
                             color: "white",
                         }}
-
                     >
                         <h5 className="titulo-glow2 pb-3 ">Datos de Contacto</h5>
-
                         <input
                             type="text"
                             placeholder="Nombre"
                             name="user_name"
                             className="form-control bg-transparent border-0 border-bottom text-white mb-4 rounded-0"
                         />
-
                         <input
                             type="text"
                             placeholder="Apellido"
                             name="last_name"
                             className="form-control bg-transparent border-0 border-bottom text-white mb-4 rounded-0"
                         />
-
                         <input
                             type="text"
                             placeholder="Telefono"
                             name="user_phone"
                             className="form-control bg-transparent border-0 border-bottom text-white mb-4 rounded-0"
                         />
-
                         <input
                             type="email"
                             placeholder="Email"
                             name="user_email"
                             className="form-control bg-transparent border-0 border-bottom text-white mb-4 rounded-0"
                         />
-
                         <Form.Control
                             as="textarea"
                             rows={3}
@@ -107,8 +98,6 @@ const Contacto = () => {
                             name="message"
                             className="bg-transparent text-white border-0 border-bottom rounded-0 mb-4"
                         />
-
-
                         <button
                             className="btn w-100 py-2 mt-2 boton-animado"
                             type="submit"
@@ -120,75 +109,55 @@ const Contacto = () => {
                                 color: "white",
                                 fontWeight: "600",
                             }}
-                        >
-                            Enviar
-                        </button>
+                        >Enviar</button>
                     </form>
-
                 </div>
             </div >
-
-            <div className="container col- 12 mt-5 mb-5">
-                <div className='col-6 text-center '
+            <div className="container d-flex justify-content-center mt-5">
+                <div className="row shadow-lg rounded-4 overflow-hidden"
                     style={{
-                        maxWidth: "500px",
-                        border: "3px white solid",
-                        borderRadius: "10px",
-                        padding: "20px"
+                        width: "75%",
+                        maxWidth: "800px",
+                        background: "rgba(0, 0, 0, 0.4)",
+                        border: "2px dashed yellow",
+                        borderRadius: "15px",
+                        height: "350px",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        color: "white",
                     }}
                 >
-
-                    <div className="d-flex align-items-center text-white mb-3 flex-wrap">
-
-                        <span className="fw-semibold me-3" style={{ whiteSpace: "nowrap" }}>
-                            LUNES A VIERNES
-                        </span>
-                        <div
-                            style={{
-                                flexGrow: 1,
-                                height: "2px",
-                                backgroundColor: "white",
-                                opacity: 0.5
-                            }}
-                        ></div>
-                        <span className="fw-bold ms-3" style={{ whiteSpace: "nowrap" }}>
-                            10:00 / 19:00
-                        </span>
-                    </div>
-                    <div className="d-flex align-items-center text-white mb-3 flex-wrap">
-                        <span className="fw-semibold me-3" style={{ whiteSpace: "nowrap" }}>
-                            SÁBADOS
-                        </span>
-                        <div
-                            style={{
-                                flexGrow: 1,
-                                height: "2px",
-                                backgroundColor: "white",
-                                opacity: 0.5
-                            }}
-                        ></div>
-                        <span className="fw-bold ms-3" style={{ whiteSpace: "nowrap" }}>
-                            10:00 / 18:00
-                        </span>
-                    </div>
-
-                </div>
-                <div className='col-' style={{
-                    maxWidth: "500px",
-                    border: "3px white solid",
-                    borderRadius: "10px",
-                    padding: "20px",
-                    marginLeft: "auto",
-                    marginRight: "20px"  // opcional
-                }}
-                >
-                    <div className="d-flex align-items-center text-white mb-3 flex-wrap">
-                        <p>METODOS DE CONTACTO</p>
-
-                    </div>
-
+                    Aquí irá el mapa
                 </div>
             </div>
+            <div className="container mt-4 d-flex justify-content-center">
+                <div
+                    style={{
+                        width: "75%",
+                        maxWidth: "800px",
+                        color: "white",
+                        textAlign: "center",
+                    }}
+                >
+                    <h3 className="fw-bold mb-3">HORARIOS</h3>
+
+                    <h5 className="fw-semibold mb-1">Lunes a Viernes</h5>
+                    <p className="m-0">09:00 a 12:00</p>
+                    <p className="m-0">17:00 a 20:00</p>
+
+                    <h5 className="fw-semibold mt-3 mb-1">Sábados</h5>
+                    <p className="m-0">09:00 a 13:00</p>
+
+
+                    <h4 className="fw-bold mt-4 mb-2">Métodos de Contacto</h4>
+
+                    <p className="mb-1">📱 <strong>WhatsApp:</strong> 3813199018</p>
+                    <p className="mb-0 pb-5">✉️ <strong>Email:</strong> Ianierogiovanna@gmail.com</p>
+                </div>
+            </div>
+
+
         </>
     );
 };
