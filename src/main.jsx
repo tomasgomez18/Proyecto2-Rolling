5aca0807322efb8ccaf5a249eb13fab97d74cd50
MatiasLazarte <matiasnicolaslazarte@gmail.com>
2025-11-28 18:37:09 -0300

    fix: arregla importacion de RoyaLToast.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Toaster } from "react-hot-toast";
 import App from "./App.jsx";
-import "./RoyalToast.css"; 
+import "./RoyaLToast.css"; 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
