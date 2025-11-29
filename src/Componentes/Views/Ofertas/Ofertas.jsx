a1f44052c1f6e64c7e8cf8986a244fda21d567d4
JuanPablo <jpsvergara28@gmail.com>
2025-11-29 12:36:16 -0300

    feat: crea vista principal del módulo de ofertas

--- src/Componentes/Views/Ofertas/Ofertas.jsx
+++ src/Componentes/Views/Ofertas/Ofertas.jsx
@@ -13,7 +13,7 @@ const productosEnOferta = [
         precioOriginal: 1450000,
         precioOferta: 1190000,
         imagen: "/img/mt07.jpg",
-        finOferta: new Date().getTime() + (1000 * 60 * 60 * 3) // 3 horas
+        finOferta: new Date().getTime() + (1000 * 60 * 60 * 3) 
     },
     {
         id: 2,
@@ -22,7 +22,7 @@ const productosEnOferta = [
         precioOriginal: 980000,
         precioOferta: 849000,
         imagen: "/img/cb300f.jpg",
-        finOferta: new Date().getTime() + (1000 * 60 * 60 * 3) // 3 horas
+        finOferta: new Date().getTime() + (1000 * 60 * 60 * 3) 
     },
     {
         id: 3,
@@ -31,7 +31,7 @@ const productosEnOferta = [
         precioOriginal: 1350000,
         precioOferta: 1090000,
         imagen: "/img/z400.jpg",
-        finOferta: new Date().getTime() + (1000 * 60 * 60 * 3) // 3 horas
+        finOferta: new Date().getTime() + (1000 * 60 * 60 * 3) 
     }
 ];
 
