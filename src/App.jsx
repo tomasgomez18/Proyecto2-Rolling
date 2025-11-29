9ebfaff9d670d4946b940376c4b0fd6905bdfac4
tomas <tomasgomezzi36@gmail.com>
2025-11-29 12:33:36 -0300

    Chore: Cambios sin importancia

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
-import { useState } from 'react'
-import { BrowserRouter,Routes,Route } from "react-router-dom"
-import Menu from "./Componentes/Shared/Menu/Menu"
-import { Home } from './Componentes/Views/Home/Home'
-import './App.css'
-import Productos from './Componentes/Views/Productos/Productos'
-import Compras from './Componentes/Views/Productos/ComponenteCompra/Compras'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Menu from "./Componentes/Shared/Menu/Menu.jsx";
+import { Home } from "./Componentes/Views/Home/Home.jsx";
+import "./App.css";
+import Productos from "./Componentes/Views/Productos/Productos.jsx";
+import Compras from "./Componentes/Views/Productos/ComponenteCompra/Compras.jsx";
+import Protecciones from "./Componentes/Views/pages/pages-menu-principal/Protecciones.jsx";
+import Indumentaria from "./Componentes/Views/pages/pages-menu-principal/PaginaCategoria.jsx";
+import Taller from "./Componentes/Views/pages/pages-menu-principal/Taller.jsx";
+import Motocicletas from "./Componentes/Views/pages/pages-menu-principal/Motocicletas.jsx";
+import PaginaCategoria from "./Componentes/Views/pages/pages-menu-principal/PaginaCategoria.jsx";
 
 function App() {
-
   return (
     <>
-    <BrowserRouter>
-       <Menu />
-       <Routes>
-         <Route path="/" element={<Home/>} />
-         <Route path="/compras" element={<Compras/> }/>
-        <Route path="/protecciones" element={<Protecciones/>} />
-       <Route path="/indumentaria" element={<Indumentaria/>} />
-      <Route path="/taller" element={<TallerPage />} />
-       </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/compras" element={<Compras />} />
+          <Route path="/compras" element={<PaginaCategoria />} />
+        
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
