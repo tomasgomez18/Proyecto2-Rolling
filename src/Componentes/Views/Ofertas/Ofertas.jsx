84b690906a7de4ca9b145307518dec33bab2392f
JuanPablo <jpsvergara28@gmail.com>
2025-11-29 12:33:18 -0300

    feat: crea vista principal del módulo de ofertas

--- src/Componentes/Views/Ofertas/Ofertas.jsx
+++ src/Componentes/Views/Ofertas/Ofertas.jsx
@@ -1,21 +1,59 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 import "./Ofertas.css";
 import OfertaItem from "./OfertaItem";
 
-const Ofertas = () => {
-    return (
-        <Container className="ofertas-container">
-            <h1 className="ofertas-title">Ofertas y Promociones</h1>
+const productosEnOferta = [
+    {
+        id: 1,
+        marca: "Yamaha",
+        modelo: "MT-07",
+        precioOriginal: 1450000,
+        precioOferta: 1190000,
+        imagen: "/img/mt07.jpg",
+        finOferta: new Date().getTime() + (1000 * 60 * 60 * 3) // 3 horas
+    },
+    {
+        id: 2,
+        marca: "Honda",
+        modelo: "CB 300F",
+        precioOriginal: 980000,
+        precioOferta: 849000,
+        imagen: "/img/cb300f.jpg",
+        finOferta: new Date().getTime() + (1000 * 60 * 60 * 3) // 3 horas
+    },
+    {
+        id: 3,
+        marca: "Kawasaki",
+        modelo: "Z400",
+        precioOriginal: 1350000,
+        precioOferta: 1090000,
+        imagen: "/img/z400.jpg",
+        finOferta: new Date().getTime() + (1000 * 60 * 60 * 3) // 3 horas
+    }
+];
 
-            <Row className="ofertas-grid">
-               
-                <Col xs={12} md={6} lg={4}>
-                    <OfertaItem />
-                </Col>
-            </Row>
-        </Container>
+function Ofertas() {
+    return (
+        <section className="ofertas-container">
+            <Container>
+                <h2>Ofertas Especiales</h2>
+                <p>
+                    Promociones por tiempo limitado en motocicletas seleccionadas
+                </p>
+
+                <Row className="mt-4">
+                    {productosEnOferta.map((producto) => (
+                        <Col key={producto.id} lg={4} md={6} className="mb-4">
+                            <OfertaItem producto={producto} />
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+        </section>
     );
-};
+}
 
 export default Ofertas;
