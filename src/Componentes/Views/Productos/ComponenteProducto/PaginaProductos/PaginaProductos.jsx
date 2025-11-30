de07aa1baf13dd1b0856460d4998aab8d5c6458c
tomas <tomasgomezzi36@gmail.com>
2025-11-30 20:52:00 -0300

    Feat: Agrega logica a la paginaProductos

--- src/Componentes/Views/Productos/ComponenteProducto/PaginaProductos/PaginaProductos.jsx
+++ src/Componentes/Views/Productos/ComponenteProducto/PaginaProductos/PaginaProductos.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import './PaginaProductos.css'
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useProductos } from '../Context/ContextoProducto';
+import BuscadorProducto from './BuscadorProducto';
+import ListaProductos from './ListaProductos';
+import './PaginaProductos.css';
 
 const PaginaProductos = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const location = useLocation();
+  const { actualizarFiltros, filtros } = useProductos();
 
-export default PaginaProductos
+  // Obtener la categoría desde la navegación y aplicarla como filtro
+  useEffect(() => {
+    if (location.state?.categoriaSeleccionada) {
+      actualizarFiltros({ 
+        categoria: location.state.categoriaSeleccionada 
+      });
+    }
+  }, [location.state, actualizarFiltros]);
+
+  return (
+    <div className="contenedor-pagina-productos">
+      <div className="encabezado-productos">
+        <h1 className="titulo-pagina">
+          {filtros.categoria 
+            ? `Productos - ${filtros.categoria.charAt(0).toUpperCase() + filtros.categoria.slice(1)}`
+            : 'Todos nuestros productos'
+          }
+        </h1>
+        <p className="descripcion-pagina">
+          Encuentra exactamente lo que necesitas para tu moto
+        </p>
+      </div>
+
+      <BuscadorProducto />
+      
+      <ListaProductos />
+    </div>
+  );
+};
+
+export default PaginaProductos;
