0801012b6900cabbee619cb0f0ca3284ac862bf7
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-12-01 03:58:48 -0300

    feat: Añade una galeria de imagenes de la empresa acorde al estilo de la misma.
    Añade:
    Carpeta galeria dentro de Home.
    galeria: Galeria.jsx y Galeria.css

--- src/Componentes/Views/Home/Home.jsx
+++ src/Componentes/Views/Home/Home.jsx
@@ -2,10 +2,14 @@
 import React from 'react'
 import Ofertas from "../Productos/Ofertas/Ofertas";
 import Producto from '../Productos/Productos';
+import Galeria from './galeria/galeria';
+import Portada from './portada/Portada';
 
 const Home = () => {
   return (
     <div>
+      <Portada/>
+      <Galeria/>
       <Producto/>
     </div>
   )
