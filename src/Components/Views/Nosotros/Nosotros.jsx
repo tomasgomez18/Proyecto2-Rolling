a2334acb40d6875a70686126486bc893c6504317
Giovanna Ianiero <Ianierogiovanna@gmail.com>
2025-12-01 12:23:20 -0300

    realiza estructura de historia de Rolling Motors

--- src/Components/Views/Nosotros/Nosotros.jsx
+++ src/Components/Views/Nosotros/Nosotros.jsx
@@ -1,12 +1,28 @@
-import './Nosotros.css'
+import React from "react";
+import "./Nosotros.css";
 
 const Nosotros = () => {
-  return (
-    <div>
-        <h1>HOLA</h1>
-      
-    </div>
-  )
-}
+    return (
+        <section className="nosotros-section">
+            <div className="nosotros-container">
+                <div className="info-card">
+                    <h6 className="card-title"> Somos Rolling Motors</h6>
 
-export default Nosotros
+                    <h3 className="small-title">Historia</h3>
+                    <p>Nacimos de la pasión de 4 amigos por las Royal Enfield, ofreciendo calidad y confianza.
+                        Nuestra misión es brindar motocicletas premium con atención personalizada y
+                        Ser líderes en motos premium, reconocidos por nuestra pasión y compromiso.
+                    </p>
+
+                    <h3 className="small-title">Valores</h3>
+                    <p>Pasión, innovación, honestidad y excelencia.</p>
+
+                    <h3 className="small-title">Logros</h3>
+                    <p>Concesionario Royal Enfield del año 2023 y más de 500 motos vendidas.</p>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Nosotros;
