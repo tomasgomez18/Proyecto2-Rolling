c88ca3eb9609a30b41545d886a4dd48fa23cf9a1
tomas <tomasgomezzi36@gmail.com>
2025-12-03 03:45:29 -0300

    Feat: Agrega componente footer y corrige estructura de carpeta

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter,Routes,Route } from "react-router"
 import Menu from "./Componentes/Shared/Menu/Menu"
 import { Home } from './Componentes/Views/Home/Home'
 import './App.css'
-import Footer from "./Componentes/Views/Footer/Footer";
+import Footer from "./Componentes/Shared/Footer/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
